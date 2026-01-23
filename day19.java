1c3c903f23659fbd0ef868fab8717f86b34c44ab
Mariya-2 <230701180@rajalakshmi.edu.in>
2026-01-23 18:16:27 +0530

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int l=0, j=0;
+        for(int i=1;i<n+1;i++){
+            Arrays.sort(a,l,i);
+            j++;
+            if(j==3){
+                j=0;
+                l=l+3;
+            }
+        }
+        for(int x:a)
+           System.out.print(x+" ");
+    }
+}
